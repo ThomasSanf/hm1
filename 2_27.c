19aa517ea62d4c7eae9355289885f5b9e8e0fae1
Thomas Sanfourche <58660377+ThomasSanf@users.noreply.github.com>
2023-12-29 15:33:06 +0800

    Create 2_27.c

---
+++ 2_27.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    printf("    *\n");
+    printf("   ***\n");
+    printf("  *****\n");      //multiple printf
+    printf(" *******\n");
+    printf("*********\n\n");
+
+    printf("    *\n   ***\n  *****\n *******\n*********"); //single printf
+}
