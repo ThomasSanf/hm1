a26ad040da5601e7253996774802dd28af60c76d
Thomas Sanfourche <58660377+ThomasSanf@users.noreply.github.com>
2023-12-29 15:26:56 +0800

    Create 2_21.c

---
+++ 2_21.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    printf("*********\n*\t\t*\n*\t\t*\n*\t\t*\n*\t\t*\n*\t\t*\n*\t\t*\n*\t\t*\n*********\n\n");
+
+    printf("   ***\n *     *\n*       *\n*       *\n*       *\n*       *\n*       *\n *     *\n   ***\n\n");
+
+    printf("  *\n ***\n*****\n  *\n  *\n  *\n  *\n  *\n  *\n\n");
+
+    printf("    *\n   * *\n  *   *\n *     *\n*       *\n *     *\n  *   *\n   * *\n    *");
+}
